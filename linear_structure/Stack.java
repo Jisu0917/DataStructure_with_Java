51631ab54061cc000778838582846d180220f6ba
Jisu0917 <bjisu8099@gmail.com>
2024-08-26 14:08:56 +0900

    제네릭을 사용한 만능 스택 구현

--- linear_structure/Stack.java
+++ linear_structure/Stack.java
@@ -1,45 +1,55 @@
-// 정수형 자료를 담는 스택 클래스 구현
 package linear_structure;
 
-public class Stack {
-	int size = 500;  // 스택 사이즈 지정
-	int[] arr = new int[size];  // 정수형 배열 선언
-	int length = 0;  // 현재 스택의 길이 저장
-	
-	public int push(int n) {  // 파라미터 n을 스택의 최상단에 추가
-		if (length < size) {
-			arr[length] = n;
-			length++;
-			return 0;
-		} else {
-			return -1;
-		}
-	}
-	
-	public int pop() {  // 스택의 최하단에 있는 정수형 자료를 제거
-		if (length > 0) {
-			length--;
-			return arr[length];
-		} else {
-			return -1;
-		}
-	}
-	
-	public int peek() {  // 스택의 최하단에 있는 정수형 자료를 반환 (제거x)
-		if (length > 0) {
-			return arr[length - 1];
-		} else {
-			return -1;
-		} 
-	}
-	
-//	public static void main(String[] args) {
-//		Stack stack = new Stack();
-//		stack.push(1);
-//		stack.push(2);
-//		stack.push(3);
-//		System.out.println(stack.pop());  // '3'
-//		System.out.println(stack.pop());  // '2'
-//		System.out.println(stack.pop());  // '1'
-//	}
+public class Stack<T> {
+    int size = 500;  // 스택의 크기
+    Object[] arr = new Object[size];  // 만능 타입 배열 선언
+    int length = 0;  // 스택의 현재 길이
+
+    public T push(T element) {
+        if (length < size) {
+            arr[length] = element;
+            length++;
+            return element;
+        } else {
+            return null;  // 스택이 꽉 찼을 때 null 반환
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    public T pop() {
+        if (length > 0) {
+            length--;
+            return (T) arr[length];  // Object를 T로 캐스팅
+        } else {
+            return null;  // 스택이 비어있을 때 null 반환
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    public T peek() {
+        if (length > 0) {
+            return (T) arr[length - 1];  // Object를 T로 캐스팅
+        } else {
+            return null;  // 스택이 비어있을 때 null 반환
+        }
+    }
+
+    public static void main(String args[]) {
+        Stack<Integer> stack_int = new Stack<Integer>();
+        Stack<String> stack_str = new Stack<String>();
+
+        stack_int.push(1);
+        stack_int.push(2);
+        stack_int.push(3);
+        System.out.println(stack_int.pop());  // '3'
+        System.out.println(stack_int.pop());  // '2'
+        System.out.println(stack_int.pop());  // '1'
+
+        stack_str.push("One");
+        stack_str.push("Two");
+        stack_str.push("Three");
+        System.out.println(stack_str.pop());  // 'Three'
+        System.out.println(stack_str.pop());  // 'Two'
+        System.out.println(stack_str.pop());  // 'One'
+    }
 }
