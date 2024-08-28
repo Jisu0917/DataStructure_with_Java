21acefce5fae5758d2d0960f5220bb865178bbc1
Jisu0917 <bjisu8099@gmail.com>
2024-08-28 15:49:32 +0900

    삽입정렬

---
+++ sort_algorithm/InsertSort.java
@@ -0,0 +1,25 @@
+package sort_algorithm;
+
+import java.util.Arrays;
+
+public class InsertSort {
+	/*
+	public static void main(String[] args) {
+		int[] arr = {4, 8, 6, 48, 64, 86, -4, -8, -6};
+		
+		for (int i = 1; i < arr.length; i++) {
+			int key = arr[i];
+			int j = i - 1;
+			
+			// 정렬된 부분을 뒤에서부터 탐색하며 key를 삽입할 위치를 찾는다.
+			while (j >= 0 && arr[j] > key) {
+				arr[j + 1] = arr[j];
+				j--;
+			}
+			arr[j + 1] = key;
+		}
+		
+		// 삽입 정렬된 arr 출력
+		System.out.println(Arrays.toString(arr));
+	}*/
+}
