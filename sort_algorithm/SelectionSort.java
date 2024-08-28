8622426ceb864b9d658350ecc7c8f95102f601ab
Jisu0917 <bjisu8099@gmail.com>
2024-08-28 15:32:31 +0900

    선택정렬

---
+++ sort_algorithm/SelectionSort.java
@@ -0,0 +1,28 @@
+package sort_algorithm;
+
+import java.util.Arrays;
+
+public class SelectionSort {
+	/*
+	public static void main(String[] args) {
+		int[] arr = {4, 8, 6, 48, 64, 86, -4, -8, -6};
+		
+		for (int i = 0; i < arr.length - 1; i++) {
+			int min_index = i;
+			for (int j = i + 1; j < arr.length; j++) {
+				if (arr[min_index] > arr[j]) {
+					min_index = j;
+				}
+			}
+			// 현재 인덱스 i와 min_index의 값을 교환
+			if (min_index != i) {
+				int tmp = arr[i];
+				arr[i] = arr[min_index];
+				arr[min_index] = tmp;
+			}
+		}
+		
+		// 선택 정렬된 arr 출력
+		System.out.println(Arrays.toString(arr));
+	}*/
+}
