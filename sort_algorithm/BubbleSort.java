6c6161789b421ab2748b1e662ee1a4ea92058ed8
Jisu0917 <bjisu8099@gmail.com>
2024-08-28 13:30:19 +0900

    버블정렬

---
+++ sort_algorithm/BubbleSort.java
@@ -0,0 +1,24 @@
+package sort_algorithm;
+
+import java.util.Arrays;
+
+public class BubbleSort {
+	/*
+	public static void main(String[] args) {
+		int[] arr = {5, 3, 7, 8, 6, 4, 2, 1, 0, 9};
+		
+		for (int i = 0; i < arr.length - 1; i++) {
+			// 마지막 i번째 요소는 이미 정렬된 상태이므로 반복문은 arr.length - 1 - i까지 수행됩니다.
+			for (int j = 1; j < arr.length - i; j++) {
+				if (arr[j - 1] > arr[j]) {
+					int tmp = arr[j];
+					arr[j] = arr[j - 1];
+					arr[j - 1] = tmp;
+				}
+			}
+		}
+		
+		// 정렬된 배열 출력
+		System.out.println(Arrays.toString(arr));
+	}*/
+}
