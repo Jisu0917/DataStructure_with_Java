e0655b63512dd5964021c2d3cc54d8e8789cac08
Jisu0917 <bjisu8099@gmail.com>
2024-08-28 15:31:04 +0900

    문자열 버블정렬

---
+++ sort_algorithm/BubbleSortForString.java
@@ -0,0 +1,26 @@
+package sort_algorithm;
+
+import java.util.Arrays;
+
+public class BubbleSortForString {
+	/*
+    public static void main(String[] args) {
+        String[] arr = {
+            "Karina", "Suzy", "Wonyoung", "Jiwon", "Dohyun", 
+            "Minhee", "Jisu", "Dohyun", "Winter", "V"
+        };
+      
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 1; j < arr.length - i; j++) {
+                if (arr[j - 1].compareTo(arr[j]) > 0) {
+                    String tmp = arr[j];
+                    arr[j] = arr[j - 1];
+                    arr[j - 1] = tmp;
+                }
+            }
+        }
+      
+        // 버블 정렬된 arr 출력
+        System.out.println(Arrays.toString(arr));
+    }*/
+}
